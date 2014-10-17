ffa6172ad77cd0592ab25c6cb2b7934cd95bc214
danielvmpollo <danielvmpollo@gmail.com>
2014-10-17 04:34:18 -0500

    archivo que actualiza la bd cuando se edita algun campo

---
+++ tareaAldo/contro/actualizar.php
@@ -0,0 +1,16 @@
+<?php 
+	require_once './../conex/conexiones.php';
+
+	$id = $_POST['id'];
+	$user = $_POST['user'];
+	$pass = $_POST['pass'];
+	$mail = $_POST['email'];
+	echo "$id";
+
+	$editar_query = "UPDATE users set user='$user',pass='$pass',mail='$mail' WHERE id=".$id;
+
+
+	$registro_object = $conexion->query($editar_query);
+
+	header("Location: ./../index.php");
+ ?>
